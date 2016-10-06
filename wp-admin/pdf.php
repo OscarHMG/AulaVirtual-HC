412a23e9837c22c1ff0638de9c15164ad4e4ebca
Adrian Aguilar <adrian11_12@hotmail.com>
2016-10-06 15:49:22 -0500

    boton de ayuda para administrador agregado

---
+++ wp-admin/pdf.php
@@ -0,0 +1,5 @@
+<html>
+<body>
+<embed src="ayuda_administrador.pdf" width="100%" height="100%">
+</body>
+</html>
--- wp-content/plugins/wp-courseware/pages/page_course_dashboard.inc.php
+++ wp-content/plugins/wp-courseware/pages/page_course_dashboard.inc.php
@@ -13,7 +13,13 @@ function WPCW_showPage_Dashboard_load()
 {
 	$page = new PageBuilder(false);
 	$page->showPageHeader(__('Mis Cursos', 'wp_courseware'), '75%', WPCW_icon_getPageIconURL());
+
+	$form_btn_admin = '<p style="text-align:right"><input type="submit" onclic="http://www.w3schools.com" style="width:182px;height:50px;font-size:27px;margin-right:9%;" value="Ayuda Rápida" class="button-primary" ></p>';
+	/*$form_btn_admin='<form method="post" action="pdf.php" target="_blank">
+		<p style="text-align:right"><input type="submit" style="width:182px;height:50px;font-size:27px;margin-right:9%;" value="Ayuda Rápida" class="button-primary" ></p>
+	</form>';*/
 	
+	echo $form_btn_admin;
 	// Handle any deletion
 	WPCW_handler_processDeletion($page);	
 	
